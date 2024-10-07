75ca5f6f4bc279553fe036dc68e5ac795cb74a21
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-10-07 16:55:21 +0530

    add SENTRY error traking

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
-const express = require('express')
-const Agent = require('./routes/Agent')
-const Customer = require('./routes/Customer')
-const Order = require('./routes/Order')
-const Product = require('./routes/Product')
-const Return = require('./routes/Return')
-const Summary = require('./routes/Summary')
+require('./instrument.js');
 
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
+
+const Customer = require('./routes/Customer');
+const Order = require('./routes/Order');
+const Product = require('./routes/Product');
+const Return = require('./routes/Return');
+const Summary = require('./routes/Summary');
 
 const app = express();
 const port = process.env.PORT || "3000";
 
-app.use(express.json())
-app.use(cors())
 
-// app.use('/agent',Agent)
-app.use('/customer',Customer)
-app.use('/order',Order)
-app.use('/product',Product)
-app.use('/return',Return)
-app.use('/summary',Summary)
+app.use(express.json());
+app.use(cors());
 
-app.listen(port, (req,res) => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+app.use('/customer', Customer);
+app.use('/order', Order);
+app.use('/product', Product);
+app.use('/return', Return);
+app.use('/summary', Summary);
+
 
 app.get('/', (req, res) => {
     res.status(404).send('404 Not Found!');
-})
+});
 
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+
+app.use(function onError(err, req, res, next) {
+    res.status(500).send("Something went wrong!");
+});
---
+++ instrument.js
@@ -0,0 +1,15 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://8a8fe7c1d41e4ca408bebcb7e1370314@o4508081010114560.ingest.us.sentry.io/4508081010376704",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
