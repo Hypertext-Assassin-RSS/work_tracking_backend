dbd186875b042767419debce1957dc25dde58bb8
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-10-07 10:47:25 +0530

    minor update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors=require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || "3000";
 
 app.use(express.json())
 app.use(cors())
--- db/db.js
+++ db/db.js
@@ -6,15 +6,15 @@ let pool;
 
 function createPool() {
   return new Pool({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DATABASE,
+    database: process.env.DB_NAME,
     idleTimeoutMillis: 10000,
     ssl: {
       rejectUnauthorized: true,
-      ca: process.env.PGSSLCERT,
+      ca: process.env.DB_SSL_ROOT_CERT,
     },
   });
 }
